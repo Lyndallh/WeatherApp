96e0660855215c301643c4c9f195f054f4e730cd
lindi <lindi.harding@gmail.com>
2023-11-05 21:45:08 +0800

    load csv function working

--- weather.py
+++ weather.py
@@ -30,7 +30,6 @@ def convert_date(iso_string):
     Returns:
         A date formatted like: Weekday Date Month Year e.g. Tuesday 06 July 2021
     """
-    pass
 
 
 def convert_f_to_c(temp_in_farenheit):
@@ -50,30 +49,34 @@ def calculate_mean(weather_data):
     # print(sum_weather)
     # print(mean_weather)
     # print(type(mean_weather))
+    """Calculates the mean value from a list of numbers.
+
+    Args:
+        weather_data: a list of numbers. 
+        # (I heard that weather data is a list of lists)
+    Returns:
+        A float representing the mean value.
+    """
     return mean_weather
 # calculate_mean([49, 57, 56, 55, 53])
 # calculate_mean([51.0, 58.2, 59.9, 52.4, 52.1, 48.4, 47.8, 53.43])
 # calculate_mean(["51", "58", "59", "52", "52", "48", "47", "53"])
 # calculate_mean([-51, -58, -59, -52, -52, -48, -47, -53])
 
-
-    # temperatures = weather_data[1,2]
-    # weather_count = temperatures.count(temperatures)
-    # weather_sum = sum(temperatures())
-    # weather_mean = weather_sum / weather_count
-    # return weather_mean
-
-    # """Calculates the mean value from a list of numbers.
-
-    # Args:
-    #     weather_data: a list of numbers. 
-    #     # (I heard that weather data is a list of lists)
-    # Returns:
-    #     A float representing the mean value.
-    # """
-
-
 def load_data_from_csv(csv_file):
+    with open (csv_file, encoding="utf-8") as current_file:
+        reader = csv.reader(current_file)
+        next(reader)
+
+        data_list = []
+  
+        for row in reader:
+            if row:
+                row[1] = int(row[1])
+                row[2] = int(row[2])
+                data_list.append(row[:])
+
+        print(data_list)
     """Reads a csv file and stores the data in a list.
 
     Args:
@@ -81,8 +84,10 @@ def load_data_from_csv(csv_file):
     Returns:
         A list of lists, where each sublist is a (non-empty) line in the csv file.
     """
-    pass
-
+    return list(data_list)
+load_data_from_csv("WeatherApp/tests/data/example_one.csv")
+load_data_from_csv("WeatherApp/tests/data/example_two.csv")
+# load_data_from_csv("WeatherApp/tests/data/example_three.csv")
 
 def find_min(weather_data):
     """Calculates the minimum value in a list of numbers.
